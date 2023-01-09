f47a2c2a6c54853abbc38cc76f7f9f41c9729663
Toufiquer <toufiquerabc@gmail.com>
2023-01-10 03:22:45 +0600

    charts update

--- src/components/ChartName/ChartName.js
+++ src/components/ChartName/ChartName.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ChartName = ({ curr: { name, id } }) => {
   const navigate = useNavigate();
   const handleShoeCart = (id) => {
     navigate(`${id}`);
   };
+  const colorDesign = "text-xl border-2 border-solid border-blue-600 mx-4 px-4 my-2 py-2 font-semibold cursor-pointer hover:bg-blue-200";
+  const activeColor = "text-xl border-2 border-solid border-blue-600 mx-4 px-4 my-2 py-2 font-semibold cursor-pointer bg-blue-200";
+  const param = useParams();
   return (
-    <div onClick={() => handleShoeCart(id)} className="text-xl border-2 border-solid border-blue-600 mx-4 px-4 my-2 py-2 font-semibold cursor-pointer hover:bg-blue-200">
+    <div onClick={() => handleShoeCart(id)} className={id === +param.chartsID ? activeColor : colorDesign}>
       Name: {name}
     </div>
   );
