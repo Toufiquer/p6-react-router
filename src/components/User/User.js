535d63294ab1b5f32ea663056c347206abc9c1ef
Toufiquer <toufiquerabc@gmail.com>
2023-01-10 03:16:18 +0600

    users design pudate

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const User = ({ user: { name, id } }) => {
+  const param = useParams();
   const navigate = useNavigate();
   const handleUserDetails = (id) => {
     navigate(`${id}`);
   };
+  const colorDesign = "  font-bold p-2 m-2 hover:bg-blue-600 hover:text-white hover:cursor-pointer border-2 border-solid border-blue-400";
+  const activeColor = "  font-bold p-2 m-2 bg-blue-600 text-white cursor-pointer border-2 border-solid border-blue-400";
   return (
     <div>
-      <h2 className="font-bold p-2 m-2 hover:bg-blue-800 hover:text-white hover:cursor-pointer border-2 border-solid border-blue-400">
+      <h2 className={id === +param.userID ? activeColor : colorDesign}>
         <div onClick={() => handleUserDetails(id)} className="border-2 border-solid border-blue-300">
           Name: {name}, ID: {id}
         </div>
